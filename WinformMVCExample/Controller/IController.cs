2d1add63dc99efb9fbec40611505f979932ed6e9
C_hansol <zwwz11@naver.com>
2022-07-22 15:50:44 +0900

    IController 네임스페이스 변경으로 인한 커밋

--- WinformMVCExample/Controller/IController.cs
+++ WinformMVCExample/Controller/IController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WinformMVCExample.CommonInterface
+namespace WinformMVCExample.Controller
 {
     interface IController
     {
--- WinformMVCExample/Controller/UserListController.cs
+++ WinformMVCExample/Controller/UserListController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WinformMVCExample.CommonInterface;
+﻿using System.Collections.Generic;
 using WinformMVCExample.Model;
 using WinformMVCExample.View;
 
